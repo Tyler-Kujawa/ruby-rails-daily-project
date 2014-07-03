f005c208ecd588c5eca572b5ce67ba912aead96d
Ty Kujawa <comsenzce13@gmail.com>
2014-07-02 19:32:23 -0500

    Adding method to reverse string in place

--- reverse_a_string/lib/reverse_string.rb
+++ reverse_a_string/lib/reverse_string.rb
@@ -1,12 +1,19 @@
 class String
-	def reverse_str_1
+	def reverse_by_iteration!
 		str_size = self.length + 1
 		reversed_str = ""
 		str_size.times do |i|
 			current_index = self[self.length - i]
-			puts "#{current_index} is at #{i}"
 			reversed_str += current_index unless current_index.nil?
 		end
 		reversed_str
+	end
+
+	def reverse_in_place!
+		half_length = self.length/2
+		half_length.times do |i|
+			tmp = self[i]; self[i] = self[-i-1]; self[-i-1] = tmp;
+		end
+		return self
 	end
 end
--- reverse_a_string/run.rb
+++ reverse_a_string/run.rb
@@ -2,7 +2,13 @@ require_relative '../reverse_a_string/lib/reverse_string.rb'
 require 'test/unit'
 
 class TestReverseString < Test::Unit::TestCase
-	def test_reverse_string
-		assert_equal('yt', 'ty'.reverse_str_1)
+	def test_reverse_by_iteration
+		assert_equal('yt', 'ty'.reverse_by_iteration!)
+		assert_equal('jazz piano'.reverse!, 'jazz piano'.reverse_by_iteration!)
+	end
+
+	def test_reverse_in_place!
+		assert_equal('yt', 'ty'.reverse_in_place!)
+		assert_equal('jazz piano'.reverse!, 'jazz piano'.reverse_in_place!)
 	end
 end
